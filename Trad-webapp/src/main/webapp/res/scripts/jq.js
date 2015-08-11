e36d505ed180c651e4bec3f35171fac80d5475c1
Student12 <student12@cr12.coderunner.pl>
2015-08-11 10:35:37 +0200

    Adding resources

---
+++ Trad-webapp/src/main/webapp/res/scripts/jq.js
@@ -0,0 +1,10 @@
+$(function() {
+  $(".panel").hover(
+    function() {
+      $(this).css("background-color", "#ff9595");
+    },
+    function() {
+      $(this).css("background-color", "#ffffff");
+    }
+  )
+});
---
+++ Trad-webapp/src/test/java/com/invisi/snake/tests/UsersTests.java
@@ -0,0 +1,14 @@
+package com.invisi.snake.tests;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class UsersTests {
+
+	@Test
+	public void sampleTest() {
+		Assert.assertEquals(0, 0);
+	}
+	
+}
